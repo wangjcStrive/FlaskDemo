66909b1c4a2a828997ac2c4e9152e279617e43e7
jicwang <wangjc857@gmail.com>
2020-07-17 19:28:17 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, url_for, escape, render_template
+
+app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+# binding the hello function to URL /
+@app.route('/home')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='jichen'))
+    print(url_for('test_url_for'))
+    return 'test page'
+
